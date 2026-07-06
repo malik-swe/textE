9e2a53d21bd1f7dc334765821416fe606c59c616
Abdul Málik Abubacar <abdulabubacar.swe@gmail.com>
2026-07-06 18:10:13 +0200

    feat: disable raw mode at exit

--- textE.c
+++ textE.c
@@ -1,17 +1,24 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode(){
-	struct termios raw;
+struct termios orig_termios;
 
-	tcgetattr(STDIN_FILENO, &raw);
+void disbleRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
+void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
 	raw.c_lflag &= ~(ECHO);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main(){
+int main() {
 	enableRawMode();
 
 	char c;
